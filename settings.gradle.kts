add78430efd407818a978beeebbafab37c989084
matsudamper <let@matsudamper.net>
2026-03-13 03:11:35 +0900

    devのバージョンが揃ってなくて壊れているので修正

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
